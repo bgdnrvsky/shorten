868f0061a6f2de0c2e9d6ec9046db084b10818a2
bgdnrvsky <raevskijb@gmail.com>
2025-01-20 12:01:37 +0100

    fix(shortener): Handle path that starts at root
    Before, if the path started at root, 'left' parameter wouldn't be  treated correctly

--- src/decorators/shortener.rs
+++ src/decorators/shortener.rs
@@ -1,6 +1,8 @@
 use super::super::cli::PathShortenerOptions;
 use super::{Decorator, PathBuf};
 
+use std::path::Component;
+
 pub struct Shortener {
     path: PathBuf,
     parameters: PathShortenerOptions,
@@ -19,11 +21,18 @@ impl Decorator for Shortener {
     fn decorate(&self) -> PathBuf {
         let components = self.path.components().collect::<Vec<_>>();
 
-        if components.len() <= self.parameters.left + self.parameters.right {
+        let starts_with_root = components.iter().peekable().peek() == Some(&&Component::RootDir);
+        let mut left_param = self.parameters.left;
+
+        if starts_with_root {
+            left_param += 1;
+        }
+
+        if components.len() <= left_param + self.parameters.right {
             return self.path.clone();
         }
 
-        let left = PathBuf::from_iter(components.iter().take(self.parameters.left));
+        let left = PathBuf::from_iter(components.iter().take(left_param));
         let right = PathBuf::from_iter(components.iter().rev().take(self.parameters.right).rev());
 
         left.join(self.parameters.replacement.clone()).join(right)
@@ -59,6 +68,33 @@ mod tests {
         let shortener = Shortener::new(&path, parameters);
 
         assert_eq!(shortener.decorate(), path);
+    }
+
+    /// Shouldn't be an issue since the call to `components` normalizes it
+    #[test]
+    fn ends_with_separator() {
+        let parameters = PathShortenerOptions {
+            replacement: "...".to_string(),
+            left: 2,
+            right: 2,
+        };
+        let path = PathBuf::from("a/very/long/path/that/needs/to/be/cut/");
+        let shortener = Shortener::new(path, parameters);
+
+        assert_eq!(shortener.decorate(), PathBuf::from("a/very/.../be/cut"));
+    }
+
+    #[test]
+    fn starts_at_root() {
+        let parameters = PathShortenerOptions {
+            replacement: "...".to_string(),
+            left: 2,
+            right: 2,
+        };
+        let path = PathBuf::from("/a/very/long/path/that/needs/to/be/cut");
+        let shortener = Shortener::new(path, parameters);
+
+        assert_eq!(shortener.decorate(), PathBuf::from("/a/very/.../be/cut"));
     }
 
     #[test]
